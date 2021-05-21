64a57c0a0b92f5f0ff5cb7bba9195107d4f19111
Abby Work <arehmet@Abigails-MacBook-Pro.local>
2021-05-21 11:22:47 -0400

    displayed list of cakes

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
+=begin
+#displays Hello, world
 get '/' do
   "Hello, world"
+end
+=end
+
+get '/' do
+  line_num = 0
+  output = ""
+  File.open('cake.list').each do |line|
+    output += "#{line_num += 1}. #{line} <br/>"
+  end
+  output
 end
