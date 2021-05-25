233a998d3d7b8640a8e28c2678893ed8e7c02241
Abby Work <arehmet@Abigails-MacBook-Pro.local>
2021-05-25 10:15:39 -0400

    created db and ran migration for info

--- app.rb
+++ app.rb
@@ -1,28 +1,26 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'sinatra/activerecord'
+#require 'cake.rb'
+
+#class App < Sinatra::Base
+  get '/' do
+    line_num = 0
+    output = ""
+    File.open('cake.list').each do |line|
+      output += "#{line_num += 1}. #{line} <br/>"
+    end
+    output
+  end
+#end
 
 =begin
-#displays Hello, world
-get '/' do
-  "Hello, world"
-end
-=end
-
-
-get '/' do
-  line_num = 0
-  output = ""
-  File.open('cake.list').each do |line|
-    output += "#{line_num += 1}. #{line} <br/>"
-  end
-  output
-end
-
-
 get('/index') {
   erb :cakes
 }
+=end
 
+=begin
 #create a cake array from the cake file and fill
 cakes = Array.new
 cakeslist = File.new("cake.list", "r")
@@ -35,3 +33,48 @@ cakeslist.close
 get '/cakes.json' do
     json :cakelist => cakes
 end
+=end
+
+
+
+
+
+#----------------------
+#by default, show the list
+=begin
+
+get('/') {
+  @cakes = Cake.all
+  erb :index
+}
+
+#add cakes
+get('/add') {
+  @cakes = Cake.all
+  erb :add
+}
+
+#edit cakes
+get('/edit') {
+  @cakes = Cake.all
+  erb :edit
+}
+
+#delete cakes
+get('/delete') {
+  @cakes = Cake.all
+  erb :delete
+}
+
+#show cakes
+get('/show') {
+  @cakes = Cake.all
+  erb :show
+}
+
+#list of cakes
+get('/index') {
+  @cakes = Cake.all
+  erb :index
+}
+=end
